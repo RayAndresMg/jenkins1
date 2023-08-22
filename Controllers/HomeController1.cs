71238f67be1653f76e2231eb667f608fa0bbc227
RayAndresMg <86538620+RayAndresMg@users.noreply.github.com>
2023-08-22 18:06:18 -0500

    Add files via upload

---
+++ Controllers/HomeController1.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HolaMundo.Controllers
+{
+    public class HomeController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+            using System;
+            using System.Collections.Generic;
+            using System.Linq;  
+            using System.Web;  
+            using System.Web.Mvc;  {
+        namespace HelloWorldExample.Controllers
+    {
+        public class HomeController : Controller {
+            public ActionResult Index()
+            {
+                {
+                ViewBag.message = "Hello World";
+                return View();
+            }
+    }
+}
+    }
+}
